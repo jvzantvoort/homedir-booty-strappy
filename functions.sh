59cdcf65345ee27128f4e1313019f47a27520911
John van Zantvoort <john@vanzantvoort.org>
2023-08-26 18:13:19 +0200

    lint functions.sh

--- functions.sh
+++ functions.sh
@@ -21,35 +21,35 @@
 
 function die()
 {
-  printf "Fatal: %s\n" "$@"
-  exit 1
+    printf "Fatal: %s\n" "$@"
+    exit 1
 }
 
 function gh_download_url()
 {
-  local repo="$1"
-  local pattern="$2"
+    local repo="$1"
+    local pattern="$2"
 
-  curl -s https://api.github.com/repos/${repo}/releases/latest | \
-    jq -M .assets[].browser_download_url | grep "${pattern}" | sed 's,\",,g'
+    curl -s https://api.github.com/repos/${repo}/releases/latest | \
+        jq -M .assets[].browser_download_url | grep "${pattern}" | sed 's,\",,g'
 }
 
 function gh_download()
 {
-  local repo
-  local pattern
-  local url
-  local downloaddir
+    local repo
+    local pattern
+    local url
+    local downloaddir
 
-  repo="$1"
-  pattern="$2"
-  downloaddir="$3"
+    repo="$1"
+    pattern="$2"
+    downloaddir="$3"
 
-  url="$(gh_download_url "${repo}" "${pattern}")"
-  downloadfile="$(basename "${url}")"
+    url="$(gh_download_url "${repo}" "${pattern}")"
+    downloadfile="$(basename "${url}")"
 
-  curl -LSso "${downloaddir}/${downloadfile}" "${url}"
-  echo "${downloadfile}" > "${downloaddir}/lastdownload.lst"
+    curl -LSso "${downloaddir}/${downloadfile}" "${url}"
+    echo "${downloadfile}" > "${downloaddir}/lastdownload.lst"
 }
 
 function lastdownload()
@@ -59,34 +59,34 @@ function lastdownload()
 
 function mkstaging_area()
 {
-  [[ -n "${STAGING_AREA}" ]] && return
+    [[ -n "${STAGING_AREA}" ]] && return
 
-  local template
-  local retv
+    local template
+    local retv
 
 
-  template="/tmp/homedir-booty-strappy.XXXXXXXX"
+    template="/tmp/homedir-booty-strappy.XXXXXXXX"
 
-  STAGING_AREA="$(mktemp -d ${template})"
-  retv="$?"
-  return "${retv}"
+    STAGING_AREA="$(mktemp -d ${template})"
+    retv="$?"
+    return "${retv}"
 
 } # end: mkstaging_area
 
 function rmstaging_area()
 {
-  [[ -z "${STAGING_AREA}" ]] && return 0
-  rm -rvf "${STAGING_AREA}" || return 1
+    [[ -z "${STAGING_AREA}" ]] && return 0
+    rm -rvf "${STAGING_AREA}" || return 1
 }
 
 function must_have_command()
 {
-  local command="$1"
-  if which "${command}" >/dev/null 2>&1
-  then
-    return 0
-  fi
-  die "command ${command}" not found"
+    local command="$1"
+    if which "${command}" >/dev/null 2>&1
+    then
+        return 0
+    fi
+    die "command ${command}" not found"
 }
 
 
